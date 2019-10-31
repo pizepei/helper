828f4b6d7cc8bbe708f8b520d1bdd9f976a72ac4
pizepei <848733913@qq.com>
2019-10-31 23:04:10 +0800

    增加批量判断是否为空

--- src/Helper.php
+++ src/Helper.php
@@ -142,28 +142,32 @@ class Helper extends Container
      * @Author 皮泽培
      * @Created 2019/7/17 16:10
      * @param $data
-     * @param string $name
+     * @param $name
      * @title  判断是否为空
      * @explain 如果为int 0  string 0 array [] 都会返回 true
      * @return bool
      * @throws \Exception
      */
-    public function is_empty($data,string $name=''):bool
+    public function is_empty($data,$name=''):bool
     {
         if ($name == ''){
             if (empty($data) || $data === 0 || $data === '0' || $data === '' || $data===[]){
                 return true;
             }
         }else if (is_array($data) && $name !== ''){
-            /**
-             * 判断是否存在
-             */
-            if (isset($data[$name])){
-                if (empty($data[$name]) || $data[$name] === 0 || $data[$name] === '0' || $data[$name] === '' || $data[$name] === []){
+            # 判断是否存在
+            if (!is_array($name)){
+                $name = [$name];
+            }
+            foreach ($name as $key=>$value)
+            {
+                if (isset($data[$value])){
+                    if (empty($data[$value]) || $data[$value] === 0 || $data[$value] === '0' || $data[$value] === '' || $data[$value] === []){
+                        return true;
+                    }
+                }else{
                     return true;
                 }
-            }else{
-                return true;
             }
         }else{
             throw new \Exception('参数错误');
